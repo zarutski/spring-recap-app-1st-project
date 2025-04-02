ecc9b7a4e25e7030941dacf22148d849b19f8c25
Maksim Zarutski <zarutski93maksim@gmail.com>
2025-04-02 16:37:54 +0300

    db auto-init added

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,19 @@
+INSERT INTO public.person (name, year_of_birth) VALUES ('Марк Артурович Меховой', 1995);
+INSERT INTO public.person (name, year_of_birth) VALUES ('Александр Алексеевич Смеликов', 1963);
+INSERT INTO public.person (name, year_of_birth) VALUES ('Софья Олеговна Малей', 1989);
+INSERT INTO public.person (name, year_of_birth) VALUES ('Владимир Александрович Зеленый', 1945);
+INSERT INTO public.person (name, year_of_birth) VALUES ('Вера Максимовна Волейчук', 1977);
+INSERT INTO public.person (name, year_of_birth) VALUES ('Мария Андреевна Лор', 2004);
+INSERT INTO public.person (name, year_of_birth) VALUES ('Андрей Витальевич Глушень', 2001);
+INSERT INTO public.person (name, year_of_birth) VALUES ('Адрей Петрович Пыль', 1910);
+
+
+INSERT INTO public.book (title, author, publish_year) VALUES ('Старые и новые подмостки', 'Артур Боровой', 1921);
+INSERT INTO public.book (title, author, publish_year) VALUES ('После двух часов', 'Алексей Бледный', 1763);
+INSERT INTO public.book (title, author, publish_year) VALUES ('На скольки берегах', 'Мария Залейкина', 1889);
+INSERT INTO public.book (title, author, publish_year) VALUES ('Своя усадьба', 'Максим Лапин', 1677);
+INSERT INTO public.book (title, author, publish_year) VALUES ('Третий после новой республики', 'Антор Лоран', 1804);
+INSERT INTO public.book (title, author, publish_year) VALUES ('Однажды в полдень', 'Григорий Поль', 1921);
+INSERT INTO public.book (title, author, publish_year) VALUES ('Двоюродные враги', 'Пол Вилкенсон', 1933);
+INSERT INTO public.book (title, author, publish_year) VALUES ('Кружка масла для подопечных', 'Артем Большевой', 1973);
+INSERT INTO public.book (title, author, publish_year) VALUES ('Вместо половины награбленного', 'Александр Подсолнух', 1964);
